3288496c14b4ac515441925353772ff6abe036f1
LessLethal <alvintrianon@gmail.com>
2023-12-15 10:50:13 +0100

    Moved code

--- index.js
+++ index.js
@@ -21,3 +21,23 @@ function sendEmail() {
 
     alert('Your message has been sent!')
 }
+
+
+
+function toggleMode() {
+    const body = document.body;
+    body.classList.toggle('dark-mode'); // Toggle the 'dark-mode' class on the body
+  
+    // Save user preference in localStorage
+    const isDarkMode = body.classList.contains('dark-mode');
+    localStorage.setItem('darkMode', isDarkMode);
+  }
+  
+  // Load user preference from localStorage on page load
+  window.onload = function() {
+    const isDarkMode = JSON.parse(localStorage.getItem('darkMode'));
+    if (isDarkMode) {
+      document.body.classList.add('dark-mode');
+    }
+  };
+  
